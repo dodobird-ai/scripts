26610a8939376247455d5ce32d3c8b418a38565a
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2024-10-22 10:30:13 +0800

    fix: remove RUN from dockerfile copy-paste

--- install_just.sh
+++ install_just.sh
@@ -4,10 +4,9 @@ set -o errexit; set -o pipefail; set -o nounset;
 
 
 # INSTALL
-RUN curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to /usr/local/bin
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to /usr/local/bin
 
 # SETUP
-
 # Create directory for completion scripts
 mkdir -p /etc/bash_completion.d/
 
